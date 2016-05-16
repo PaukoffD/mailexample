6144e5dffdf05483824cabab633974bdb56356d7
xuliaP <paukov2967@gmail.com>
2016-05-16 17:35:55 +0300

    'initial'

---
+++ app/mailers/emailer.rb
@@ -0,0 +1,9 @@
+class Emailer < ApplicationMailer
+default from: 'xulia@paukoff.ru'
+ 
+  def welcome_email(user)
+    @user = user
+    @url  = 'http://example.com/login'
+    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+  end
+end
